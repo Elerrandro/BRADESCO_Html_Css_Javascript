687e990bf80f0749689fd642fdc4720e8773fcc6
Elerrandro <elerrandrosousa@gmail.com>
2024-09-12 18:04:56 -0300

    Add files via upload

---
+++ Site-simples/app.js
@@ -0,0 +1,17 @@
+'use strict';
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "dark";
+    }
+    else {
+        this.textContent = "light";
+    }
+
+    console.log('current class name: ' + className);
+});
+
